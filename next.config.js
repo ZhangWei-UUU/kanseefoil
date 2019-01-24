d7a1c4d7f4dcd6866ad2aa204a71237ea3bb6913
ZhangWei <350095093@qq.com>
2019-01-24 21:10:30 +0800

    test new config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const webpack = require('webpack');
 const path = require('path')
 
 const withCSS = require('@zeit/next-css')
-
+if (typeof require !== 'undefined') {
+    require.extensions['.css'] = file => {}
+  }
+  
 module.exports = withCSS()
 
 
