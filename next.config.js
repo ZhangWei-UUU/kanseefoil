416ea2c8fbf74dbd280cd2360a2efe60a8491a99
ZhangWei <350095093@qq.com>
2019-01-24 22:09:55 +0800

    add css

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const path = require('path')
 const withCSS = require('@zeit/next-css')
 if (typeof require !== 'undefined') {
     require.extensions['.css'] = file => {}
-  }
+}
   
 module.exports = withCSS()
 
