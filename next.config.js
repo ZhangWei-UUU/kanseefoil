67cf6aa79b19fb9afdeba6d92afdd1a1c3efd648
ZhangWei <350095093@qq.com>
2019-01-24 18:25:55 +0800

    delete css config

--- next.config.js
+++ next.config.js
@@ -8,33 +8,33 @@ if (typeof require !== 'undefined') {
   require.extensions['.css'] = () => {}
 }
 module.exports = withCSS({
-        onDemandEntries: {
-            // Make sure entries are not getting disposed.
-            maxInactiveAge: 1000 * 60 * 60
-          },
-          serverRuntimeConfig: {
-            mySecret: 'secret'
-          },
-          publicRuntimeConfig: {
-            staticFolder: '/static'
-          },
-        webpack(config, {buildId}) {
-            const nextLocation = path.join(require.resolve('next/package.json'), '../')
-            const nextCssNodeModulesLocation = path.join(
-               require.resolve('@zeit/next-css'),
-             '../../../node_modules'
-         )
+    //     onDemandEntries: {
+    //         // Make sure entries are not getting disposed.
+    //         maxInactiveAge: 1000 * 60 * 60
+    //       },
+    //       serverRuntimeConfig: {
+    //         mySecret: 'secret'
+    //       },
+    //       publicRuntimeConfig: {
+    //         staticFolder: '/static'
+    //       },
+    //     webpack(config, {buildId}) {
+    //         const nextLocation = path.join(require.resolve('next/package.json'), '../')
+    //         const nextCssNodeModulesLocation = path.join(
+    //            require.resolve('@zeit/next-css'),
+    //          '../../../node_modules'
+    //      )
 
-    if (nextCssNodeModulesLocation.indexOf(nextLocation) === -1) {
-      config.resolveLoader.modules.push(nextCssNodeModulesLocation)
-    }
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        'process.env.CONFIG_BUILD_ID': JSON.stringify(buildId)
-      })
-    )
-    return config
-        }
+    // if (nextCssNodeModulesLocation.indexOf(nextLocation) === -1) {
+    //   config.resolveLoader.modules.push(nextCssNodeModulesLocation)
+    // }
+    // config.plugins.push(
+    //   new webpack.DefinePlugin({
+    //     'process.env.CONFIG_BUILD_ID': JSON.stringify(buildId)
+    //   })
+    // )
+    // return config
+    //     }
       })
 
 
