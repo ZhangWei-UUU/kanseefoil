a8a7cd2302652f3b817e796d64559c690b949e91
ZhangWei <350095093@qq.com>
2019-01-25 14:47:43 +0800

    runing correctt

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /* eslint-disable */
 const webpack = require('webpack');
-const path = require('path')
 
 const withCSS = require('@zeit/next-css')
 if (typeof require !== 'undefined') {
     require.extensions['.css'] = file => {}
 }
   
-module.exports = withCSS()
+module.exports = withCSS({
+    webpack(config, options) {
+      return config
+    }
+  })
 
 
